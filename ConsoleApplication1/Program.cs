601270fd95493837f7e62d9a1e91555c9046e4aa
jjvanzon <janjoostvanzon@hotmail.com>
2018-07-28 14:31:04 +0200

    Publish on GitHub.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExecuteProgram();
+            Console.ReadKey();
+        }
+
+
+        private static void ExecuteProgram()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(GetTextToPrint(i));
+            }
+        }
+
+        private static string GetTextToPrint(int i)
+        {
+            bool isMultipleOf3 = i % 3 == 0;
+            bool isMultipleOf5 = i % 5 == 0;
+
+            if (isMultipleOf3 && isMultipleOf5)
+            {
+                return "FizzBuzz";
+            }
+            else if (isMultipleOf3)
+            {
+                return "Fizz";
+            }
+            else if (isMultipleOf5)
+            {
+                return "Buzz";
+            }
+            else
+            {
+                return i.ToString();
+            }
+        }
+    }
+}
